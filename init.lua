f033387f9157dcb1a1bf0ce3ff274c62b3096dee
lolbaiteed <dvpnmrv@gmail.com>
2025-05-14 14:37:28 +0000

    some fixes in lsp

--- init.lua
+++ init.lua
@@ -21,3 +21,7 @@ vim.opt.foldmethod=expr
 
 vim.diagnostic.config({ virtual_text = true })
 
+-- netrw config --
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
+vim.g.netrw_alto = 0
---
+++ lua/plugins/netwrw.lua
@@ -0,0 +1,10 @@
+return {
+  "prichrd/netrw.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("netrw").setup({
+        use_devicons = true,
+    })
+  end,
+}
+
